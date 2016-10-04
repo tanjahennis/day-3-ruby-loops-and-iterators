1675e9890e8a89b73cd40c4384fdca21211d2b3c
YOUR NAME <tmhennis@gmail.com>
2016-10-04 16:32:43 +0200

    Welcome and product print

--- yogastore1.rb
+++ yogastore1.rb
@@ -1,2 +1,17 @@
 #yogastore1
-puts "is it working?"
+shopping_cart = []
+
+products = [
+  {item_id: 11, name: "Yoga Mat", price: 20},
+  {item_id: 12, name: "Yoga Pants", price: 15},
+  {item_id: 13, name: "Block", price: 17},
+  {item_id: 14, name: "Yoga Towel", price: 12},
+  {item_id: 15, name: "Meditation blanket", price: 25},
+  {item_id: 16, name: "Zen Socks", price: 5},
+]
+
+puts "Welcome to Yoga Store! We have awesome stuff:"
+
+products.each do |product|
+  puts "#{product[:item_id]} - #{product[:name]}, € #{product[:price]}"
+end
